d25e45b071ea1cf072f8a4cc678a5acca67f4d11
Rodolfo Queiroz <rodolfoqueiroz@Rodolfos-MacBook-Pro.local>
2015-01-12 15:12:52 +1100

    fist rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(sender: UIButton) {
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+    }
 
 }
 
